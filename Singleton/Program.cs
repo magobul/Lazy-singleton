8943aacc7cd7d2de59b21c6731ad35dbba6e0f44
magobul <magobul@gmail.com>
2024-09-23 11:49:39 +0700

    Add files via upload

---
+++ Singleton/Program.cs
@@ -0,0 +1,29 @@
+﻿using Singleton;
+
+public class Program
+{
+    public static void Main()
+    {
+        var servers = Servers.Singleton;
+
+        Console.WriteLine(servers.AddServer("http://apple.com")); // True
+        Console.WriteLine(servers.AddServer("https://explorer.com")); // True
+        Console.WriteLine(servers.AddServer("ftp://axe.com")); // False
+        Console.WriteLine(servers.AddServer("http://apple.com")); // False (дубликат)
+
+        var httpServers = servers.GetHttpServers();
+        var httpsServers = servers.GetHttpsServers();
+
+        Console.WriteLine("HTTP Servers:");
+        foreach (var server in httpServers)
+        {
+            Console.WriteLine(server);
+        }
+
+        Console.WriteLine("HTTPS Servers:");
+        foreach (var server in httpsServers)
+        {
+            Console.WriteLine(server);
+        }
+    }
+}
---
+++ Singleton/Servers.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class Servers
+    {
+        private static readonly Lazy<Servers> singleton = new Lazy<Servers>(() => new Servers());
+        private readonly HashSet<string> serverList;
+
+        private Servers()
+        {
+            serverList = new HashSet<string>();
+        }
+
+        public static Servers Singleton = singleton.Value;
+
+        public bool AddServer(string serverAddress)
+        {
+            if (string.IsNullOrWhiteSpace(serverAddress) || (!serverAddress.StartsWith("http://") && !serverAddress.StartsWith("https://")) || !serverList.Add(serverAddress))
+            {
+                return false; // Сервер не добавлен
+            }
+
+            return true; // Сервер успешно добавлен
+        }
+
+        public List<string> GetHttpServers()
+        {
+            var httpServers = new List<string>();
+            foreach (var server in serverList)
+            {
+                if (server.StartsWith("http://"))
+                {
+                    httpServers.Add(server);
+                }
+            }
+            return httpServers;
+        }
+
+        public List<string> GetHttpsServers()
+        {
+            var httpsServers = new List<string>();
+            foreach (var server in serverList)
+            {
+                if (server.StartsWith("https://"))
+                {
+                    httpsServers.Add(server);
+                }
+            }
+            return httpsServers;
+        }
+    }
+}
